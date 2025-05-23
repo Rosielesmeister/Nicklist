fd07ee40f1d292c682029c82ed3dc86157faca96
SeanPease <159569495+SeanPease@users.noreply.github.com>
2025-05-23 12:19:34 -0400

    added concurrent npm run dev t run backend and frontend in one script, also added navbar for king alan

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react"
+import { AdvancedImage } from "@cloudinary/react"
 import { useCloudinaryImage } from "./hooks/useCloudinaryImage"
-
+import Auth from "./components/auth/Auth"
+import "bootstrap/dist/css/bootstrap.min.css"
 const App = () => {
 	const { getOptimizedImage } = useCloudinaryImage("doaflgje")
 	const img = getOptimizedImage("cld-sample-5")
 
-	return <AdvancedImage cldImg={img} />
+	return (
+		<>
+			{/* navbar */}
+			<Auth />
+
+			{/* cloudinary */}
+			{/* <AdvancedImage cldImg={img} /> */}
+		</>
+	)
 }
 
 export default App
