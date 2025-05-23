99473cb275a1a6d4d90ffcf4f4438fa985c0aa69
SeanPease <159569495+SeanPease@users.noreply.github.com>
2025-05-23 10:50:39 -0400

    components made uppercase per convention, login/registration child folder added to Auth parent

--- client/src/components/auth.jsx
+++ client/src/components/auth.jsx
@@ -1,1 +1,12 @@
-// login , register log out components
+//Auth is the parent component for the login/register components
+import React from "react"
+import Login from "./login/Login"
+import Register from "./registration/Register"
+
+// import bootstrap componets
+
+function Auth() {
+	return <div>auth</div>
+}
+
+export default Auth
