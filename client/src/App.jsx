1f5942a09ffa09467d5e6682a5cd58ff99844c07
SeanPease <159569495+SeanPease@users.noreply.github.com>
2025-05-23 09:51:35 -0400

    added custom hook folder to keep App.jsx import section as clean as possible

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,11 @@
-import React from 'react'
-import { Cloudinary } from '@cloudinary/url-gen';
-import { auto } from '@cloudinary/url-gen/actions/resize';
-import { autoGravity } from '@cloudinary/url-gen/qualifiers/gravity';
-import { AdvancedImage } from '@cloudinary/react';
+import React from "react"
+import { useCloudinaryImage } from "./hooks/useCloudinaryImage"
 
 const App = () => {
-  const cld = new Cloudinary({ cloud: { cloudName: 'doaoflgje' } });
-  
-  // Use this sample image or upload your own via the Media Explorer
-  const img = cld
-        .image('cld-sample-5')
-        .format('auto') // Optimize delivery by resizing and applying auto-format and auto-quality
-        .quality('auto')
-        .resize(auto().gravity(autoGravity()).width(500).height(500)); // Transform the image: auto-crop to square aspect_ratio
+	const { getOptimizedImage } = useCloudinaryImage("doaflgje")
+	const img = getOptimizedImage("cld-sample-5")
 
-  return (<AdvancedImage cldImg={img}/>);
-};
+	return <AdvancedImage cldImg={img} />
+}
 
 export default App
