6d32570513e3d893889809a70a0eb0b29f57c208
SeanPease <159569495+SeanPease@users.noreply.github.com>
2025-05-24 11:39:30 -0400

    merged and mounted navbar, ready to build homepage and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,64 @@
-import React from 'react';
-import { useCloudinaryImage } from "./hooks/useCloudinaryImage";
-import { AdvancedImage } from '@cloudinary/react'; // Add this import
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import CreatePost from './pages/CreatePost';
-import EditPost from './pages/EditPost';
-
-function App() {
-  const { getOptimizedImage } = useCloudinaryImage("doaflgje");
-  const img = getOptimizedImage("cld-sample-5");
-
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/create" element={<CreatePost />} />
-            <Route path="/edit/:id" element={<EditPost />} />
-            <Route path="/image" element={<AdvancedImage cldImg={img} />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
-  );
 import React from "react"
-import { AdvancedImage } from "@cloudinary/react"
 import { useCloudinaryImage } from "./hooks/useCloudinaryImage"
+import { AdvancedImage } from "@cloudinary/react" // Add this import
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+//TODO import Navbar from "./components/Navbar"
+//TODO import Home from "./pages/Home"
+//TODO import Dashboard from "./pages/Dashboard"
+//TODO import CreatePost from "./pages/CreatePost"s
+//TODO import EditPost from "./pages/EditPost"
 import Auth from "./components/auth/Auth"
 import "bootstrap/dist/css/bootstrap.min.css"
-const App = () => {
+
+function App() {
 	const { getOptimizedImage } = useCloudinaryImage("doaflgje")
 	const img = getOptimizedImage("cld-sample-5")
 
 	return (
-		<>
-			{/* navbar */}
-			<Auth />
-
-			{/* cloudinary */}
-			{/* <AdvancedImage cldImg={img} /> */}
-		</>
+		<Router>
+			<div className="min-h-screen bg-gray-50">
+				<main className="container mx-auto px-4 py-8">
+					{/* Layuout components */}
+					<Auth />
+					<div style={{ backgroundColor: "red", padding: "20px" }}>
+						<h1>APP IS WORKING</h1>
+					</div>
+					{/* Layout End */}
+					<Routes>
+						{/* TODO: Uncomment each page when completed */}
+						{/* <Route
+							path="/"
+							element={<Home />}
+						/>
+						<Route
+							path="/login"
+							element={<Login />}
+						/>
+						<Route
+							path="/register"
+							element={<Register />}
+						/>
+						<Route
+							path="/dashboard"
+							element={<Dashboard />}
+						/>
+						<Route
+							path="/create"
+							element={<CreatePost />}
+						/>
+						<Route
+							path="/edit/:id"
+							element={<EditPost />}
+						/>
+						<Route
+							path="/image"
+							element={<AdvancedImage cldImg={img} />}
+						/> */}
+					</Routes>
+				</main>
+			</div>
+		</Router>
 	)
 }
 
-export default App;
+export default App
