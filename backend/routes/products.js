463f7c54bb98a6682213236928adc0c804b91dbf
SeanPease <159569495+SeanPease@users.noreply.github.com>
2025-05-24 12:01:59 -0400

    removed authenticaiton from get products so all users can see without registering, also deleted the .jsx files in our backend

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,22 +1,21 @@
-import express from 'express'
+import express from "express"
 const router = express.Router()
 import { authenticate } from "../middleware/auth.js"
 
 import {
-  createProduct,
-  getProducts,
-  getProductById,
-  updateProduct,
-  deleteProduct,
-  getProductsByUserId,
+	createProduct,
+	getProducts,
+	getProductById,
+	updateProduct,
+	deleteProduct,
+	getProductsByUserId,
 } from "../controllers/products.js"
 
-
-router.post('/products', authenticate, createProduct);
-router.get('/products', authenticate, getProducts);
-router.get('/products/:id', authenticate, getProductById);
-router.put('/products/:id',authenticate, updateProduct);
-router.get('/user/products', authenticate, getProductsByUserId)
-router.delete("/products/:id", authenticate, deleteProduct) 
+router.post("/products", authenticate, createProduct)
+router.get("/products", getProducts)
+router.get("/products/:id", authenticate, getProductById)
+router.put("/products/:id", authenticate, updateProduct)
+router.get("/user/products", authenticate, getProductsByUserId)
+router.delete("/products/:id", authenticate, deleteProduct)
 
 export default router
