887384e0df22bf29db309cdfcca9c063bb92f8e3
pamellasprung <pmkotsadam@gmail.com>
2025-05-24 12:26:26 -0400

    add home page & app.jsx updates to allow browsing bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { useCloudinaryImage } from "./hooks/useCloudinaryImage"
 import { AdvancedImage } from "@cloudinary/react" // Add this import
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 //TODO import Navbar from "./components/Navbar"
-//TODO import Home from "./pages/Home"
+import Home from "./pages/Home"
 //TODO import Dashboard from "./pages/Dashboard"
 //TODO import CreatePost from "./pages/CreatePost"s
 //TODO import EditPost from "./pages/EditPost"
@@ -25,36 +25,37 @@ function App() {
 					</div>
 					{/* Layout End */}
 					<Routes>
-						{/* TODO: Uncomment each page when completed */}
-						{/* <Route
-							path="/"
-							element={<Home />}
-						/>
-						<Route
-							path="/login"
-							element={<Login />}
-						/>
-						<Route
-							path="/register"
-							element={<Register />}
-						/>
-						<Route
-							path="/dashboard"
-							element={<Dashboard />}
-						/>
-						<Route
-							path="/create"
-							element={<CreatePost />}
-						/>
-						<Route
-							path="/edit/:id"
-							element={<EditPost />}
-						/>
-						<Route
-							path="/image"
-							element={<AdvancedImage cldImg={img} />}
-						/> */}
-					</Routes>
+    {/* TODO: Uncomment each page when completed */}
+    <Route
+        path="/"
+        element={<Home />}
+    />
+    {/* <Route
+        path="/login"
+        element={<Login />}
+    /> */}
+    {/* <Route
+        path="/register"
+        element={<Register />}
+    /> */}
+    {/* <Route
+        path="/dashboard"
+        element={<Dashboard />}
+    /> */}
+    {/* <Route
+        path="/create"
+        element={<CreatePost />}
+    /> */}
+    {/* <Route
+        path="/edit/:id"
+        element={<EditPost />}
+    /> */}
+    {/* <Route
+        path="/image"
+        element={<AdvancedImage cldImg={img} />}
+    /> */}
+   
+</Routes>
 				</main>
 			</div>
 		</Router>
